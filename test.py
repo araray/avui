7d03e0970ff55963e03fd515b129ef7cf4319b7d
Araray Velho <araray@gmail.com>
2021-10-27 18:49:28 -0300

    Adding .gitignore.

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ a.add_window(3,x,y-3,0,'b','c')
 a.add_n_windows(6, 0, 0, 'cont')
 a.windows[0].highlight_window()
 a.windows[0].window_title('Title')
-a.wprint(1, 3, ' Aaiv ', 'a')
+a.wprint(1, 3, 'Some text', 'a')
 a.wprint(3, 3, 'Test text for testing!', 'a')
 a.windows[0].top_bar_info('*', a.msg_type['ALERT'])
 a.wprint(1, 3, ' Docker ', 'cont')
@@ -29,5 +29,4 @@ for w in a.get_win_by_tag('a'):
     a.show_win(w)
 
 a.stdscr.getch()
-map = a.win_map
 a.finalize()
